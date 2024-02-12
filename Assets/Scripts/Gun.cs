af3d23e3bc2fcf21ffe886d3a2b1c773c4ab3409
Kkk <2568143457@qq.com>
2024-02-12 16:23:03 +0800

    chore: 添加基本行为事件

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
     public bool isFire;
     public ParticleSystem particleSystem;
+    public Animator GunAnimation;
+    public PlayerInputControl InputControl;
+
+    #region 生命周期
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        InputControl = new PlayerInputControl();
+
+        InputControl.Player.Load.started += OnLoadBullet;
+        InputControl.Player.Defend.started += OnDefend;
+        InputControl.Player.Fire.started += OnFire;
     }
 
     // Update is called once per frame
@@ -18,12 +29,51 @@ public class Gun : MonoBehaviour
         if (isFire)
         {
             isFire = false;
-            Fire();
+            FireParticle();
         }
     }
-    
+
+    private void OnEnable()
+    {
+        InputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputControl.Disable();
+    }
+
+    #endregion
+ 
+
     void Fire()
+    {
+        GunAnimation.SetTrigger("Fire");
+    }
+
+    void LoadBullet()
+    {
+        GunAnimation.SetTrigger("Load");
+    }
+    
+    void FireParticle()
     {
         particleSystem.Play();   
     }
+
+    #region 用户行为
+    
+    private void OnLoadBullet(InputAction.CallbackContext obj)
+    {
+        LoadBullet();
+    } 
+    private void OnDefend(InputAction.CallbackContext obj)
+    {
+    } 
+    private void OnFire(InputAction.CallbackContext obj)
+    {
+        Fire();
+    }
+    
+    #endregion
 }
