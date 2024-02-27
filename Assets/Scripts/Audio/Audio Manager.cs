f05047dbc6a8a72871a877507aed5e511ff13e03
Kkk <2568143457@qq.com>
2024-02-27 17:36:47 +0800

    chore: 添加枪支操作音效

---
+++ Assets/Scripts/Audio/Audio Manager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip LoadClip;
+    public AudioClip ShootClip;
+    public AudioClip UltimateShootClip;
+    public RoundStartStopSO RoundStartStopSO;
+    public UIAnimationEndedSO UIAnimationEndedSO;
+    
+    private void OnEnable()
+    {
+        RoundStartStopSO.OnRoundStartStop += OnRoundStartStop;
+        UIAnimationEndedSO.OnUIAnimationEnded += OnUIAnimationEnded;
+    }
+
+    private void OnDisable()
+    {
+        RoundStartStopSO.OnRoundStartStop -= OnRoundStartStop;
+        UIAnimationEndedSO.OnUIAnimationEnded -= OnUIAnimationEnded;
+    }
+
+    private void OnUIAnimationEnded()
+    {
+        if (GameManager.Instance.isPlaying)
+            PlayAudioClip();
+    }
+
+    private void OnRoundStartStop(bool isStart, Dictionary<OperatorOption, bool> arg1, IInformation PlayerInfo)
+    {
+        if (!isStart)
+        {
+            SwitchAudioClip(PlayerInfo.GetCurrentStatus());
+        }
+    }
+
+    private void SwitchAudioClip(OperatorOption option)
+    {
+        switch (option)
+        {
+            case OperatorOption.LOAD: AudioSource.clip = LoadClip; break;
+            case OperatorOption.SHOOT: AudioSource.clip = ShootClip; break;
+            case OperatorOption.ULTIMATE_SHOOT: AudioSource.clip = UltimateShootClip; break;
+        }
+    }
+
+    void PlayAudioClip()
+    {
+        AudioSource.Play();
+    }
+}
